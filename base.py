99e3fe3262f4ab8a951054150240dbafc03e9913
Corey Osman <corey@logicminds.biz>
2011-03-15 17:59:59 -0400

    Added initial test code and base.py module for pypuppet

---
+++ base.py
@@ -0,0 +1,89 @@
+import pycurl
+import StringIO
+import yaml
+
+
+class pypuppet:
+
+    # Setup the connection object with curl
+    conn = pycurl.Curl()
+    cert="/etc/puppetlabs/puppet/ssl/certs/cobbler.logicminds.corp.pem"
+    key="/etc/puppetlabs/puppet/ssl/private_keys/cobbler.logicminds.corp.pem"
+    host = 'https://puppet.logicminds.corp'
+    port = '8140'
+    url = "https://puppet.logicminds.corp:8140/production/facts/cobbler.logicminds.corp"
+    rawyaml = StringIO.StringIO()
+    
+    #def __init__(self):
+        
+       
+    def sethost(self,host, port):
+        # set the host if you need to set it after creating the object
+        self.host = host
+        self.port = port
+        self.connect()
+
+    def setenvironment(self,environment):
+        self.environment = environment
+        self.connect()
+
+    def setresource(self,resource):
+        self.resource = resource
+        self.connect()
+
+    def setkey(self,key):
+        self.key = key
+        self.connect()
+                
+    def connect(self,host=None, port=None, environment=None, resource=None, key=None):
+       # This function is called by many of the set functions in order to re-create the url pattern
+       # Build connection string url
+      
+       self.host = host
+       self.port = port
+       self.environment = environment
+       self.resource = resource
+       self.key = key
+       # We will need to test all of these attributes before moving forward
+       # create error if any of these are still undefined 
+       # Update url and pycurl object 
+       self.createurl()
+       self.setcurlattrs()
+       
+    def createurl(self):
+       self.url = "https://%s:%s/%s/%s/%s" % (self.host,self.port,self.environment,self.resource,self.key)
+       print self.url
+       self.conn.setopt(self.conn.URL, self.url)
+       
+    def decode(self):
+        # Need to check for certs
+        
+        self.conn.setopt(self.conn.WRITEFUNCTION, self.rawyaml.write)
+        self.conn.perform()
+        self.conn.close()
+        # Below is the yaml encoded output from puppet
+#        contents = self.rawyaml.getvalue()
+        # Process the contents and Return the contents in a python object
+#        return yaml.load(contents)
+
+    def setcerts(self,cert, key):
+        # Include the path to the cert and path to the private key
+        # If these Certs are not set we cannot get any data out of puppet
+        self.cert = cert
+        self.key = key
+        # Lets put some cert verfication here
+        self.conn.setopt(self.conn.SSL_VERIFYPEER, 0)
+        self.conn.setopt(self.conn.SSL_VERIFYHOST, 0)
+        self.conn.setopt(self.conn.SSLCERT,self.cert)
+        self.conn.setopt(self.conn.SSLKEY,self.key)
+
+    def setcurlattrs(self,format='yaml'):
+        # Setup the output to Yaml
+        # setopt doesn't seem to like passing in strings
+        formatstring = '[\"Accept: %s\"]' % (format)
+        self.conn.setopt(self.conn.HTTPHEADER, ["Accept: yaml"])
+        #self.conn.setopt(self.conn.VERBOSE,1)
+        #c.setopt(conn.ENCODING, 'Accept: yaml')
+
+        # So far this is the only attribute I can think of
+
---
+++ pypuppet-test.py
@@ -0,0 +1,23 @@
+from base import pypuppet
+
+
+# Create Test of functions
+
+# You can either pass in all the connection info at object creation time or via connect and other set commands
+cert="/etc/puppetlabs/puppet/ssl/certs/cobbler.logicminds.corp.pem"
+key="/etc/puppetlabs/puppet/ssl/private_keys/cobbler.logicminds.corp.pem"
+
+# Create Object
+puppet = pypuppet()
+
+# Set the connection options
+puppet.connect('puppet.logicminds.corp', '8140', 'production', 'facts', 'puppetagent1.logicminds.corp')
+
+# Set the certificates
+puppet.setcerts(cert,key)
+
+# Decode the output from puppet into a python object
+output = puppet.decode()
+
+print output
+print puppet.rawyaml.getvalue()
